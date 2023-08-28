80a02a7810d0a36dd9bd29211de6aa4cc84a3252
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-08-28 22:34:07 +0100

    added hooks to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 import ContactForm from 'components/ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from 'components/SearchFilter/SearchFilter';
@@ -10,99 +11,70 @@ import {
 } from './App.styled';
 import { nanoid } from 'nanoid';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export function App() {
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  // adding to and downloading from local storage
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const updateContacts = JSON.parse(contacts);
-
-    if (updateContacts) {
-      this.setState({ contacts: updateContacts });
-    }
-    console.log('App componentDidMount');
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-    console.log('App componentsDidUpdate');
-  }
-
-  //adding to and downloading from local storage
-
-  formSubmitData = data => {
-    const currentName = this.state.contacts.find(
+  const formSubmitData = data => {
+    const currentName = contacts.find(
       item => item.name.toLowerCase() === data.name.toLowerCase()
     );
     if (currentName) {
       return alert(currentName.name + 'is already in contacts');
     }
-
-    const contact = { ...data, id: nanoid() };
-    this.setState(prev => ({ contacts: [contact, ...prev.contacts] }));
+    setContacts(prevState => [...prevState, { ...data, id: nanoid() }]);
   };
 
-  handleAddContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+  // const handleAddContact = newContact => {
+  //   setContacts(prevContacts => [...prevContacts, newContact]);
+  // };
+
+  const deleteContacts = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  deleteContacts = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const changeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  changeFilter = e => {
-    this.setState({
-      filter: e.currentTarget.value,
-    });
-  };
-
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    return (
-      <Wrapper>
-        <div className="phonebook">
-          <Heading>Phonebook</Heading>
-          <ContactForm onSubmit={this.formSubmitData} />
-          <ContactsTitle>Contacts</ContactsTitle>
-          <FindContactByName>Find contacts by name</FindContactByName>
-          <Filter value={filter} onChange={this.changeFilter} />
-          {contacts.length ? (
-            <ContactList
-              contacts={this.getFilteredContacts()}
-              onDelete={this.deleteContacts}
-            />
-          ) : (
-            <p>No any contacts</p>
-          )}
-        </div>
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper>
+      <div className="phonebook">
+        <Heading>Phonebook</Heading>
+        <ContactForm onSubmit={formSubmitData} />
+        <ContactsTitle>Contacts</ContactsTitle>
+        <FindContactByName>Find contacts by name</FindContactByName>
+        <Filter value={filter} onChange={changeFilter} />
+        {contacts.length ? (
+          <ContactList
+            contacts={getFilteredContacts()}
+            onDelete={deleteContacts}
+          />
+        ) : (
+          <p>No any contacts</p>
+        )}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default App;
